1c3a4bf4195d6a17ace131585854398d7731c424
gfereggia <german@dya.com.ar>
2018-10-12 17:20:50 -0300

    async files

--- src/app/index.js
+++ src/app/index.js
@@ -1,77 +1,12 @@
-// import React from "react";
-// import {render} from "react-dom";
+import {render} from "react-dom";
+import React from "react";
+import {Provider} from "react-redux";
 
-// import { User } from './components/User';
-// import { Main } from './components/Main';
+import App from "./containers/App";
+import store from "./store";
 
-// class App extends React.Component {
-//     constructor() {
-//         super();
-//         this.state = {
-//             username: "Max"
-//         };
-//     }
-
-//     changeUsername(newName) {
-//         this.setState({
-//             username: newName
-//         });
-//     }
-
-//     render() {
-//         return (
-//             <div className="container">
-//                 <Main changeUsername={this.changeUsername.bind(this)}/>
-//                 <User username={this.state.username}/>
-//             </div>
-//         );
-//     }
-// }
-
-// render(<App />, window.document.getElementById('app'));
-
-import { createStore } from "redux";
-
-const initialState = {
-    result: 1,
-    lastValues : []
-};
-
-const reducer = (state = initialState, action) => {
-    
-    switch (action.type){
-        case "ADD":
-            state = {
-                ...state,
-                result: state.result + action.payload,
-                lastValues: [...state.lastValues, action.payload]
-            };
-            break;
-        case "SUBTRACT":
-            state = {
-                ...state,
-                result: state.result - action.payload
-                lastValues: [...state.lastValues, action.payload]
-            };
-            break;
-    }
-
-    return state;
-
-};
-
-const store = createStore(reducer);
-
-store.subscribe( () => {
-    console.log("Store updated! ", store.getState());
-});
-
-store.dispatch({
-    type: "ADD",
-    payload: 10
-});
-
-store.dispatch({
-    type: "SUBTRACT",
-    payload: 15
-});
+render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    window.document.getElementById('app'));
